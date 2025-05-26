177248985e139b75c9ad21140df4a86c72d40e13
Naufaldi Alfaghani <naufaldialfaghani25092005@gmail.com>
2025-05-26 22:10:56 +0700

    VIPMember.java

--- aplikasi golf/src/VIPMember.java
+++ aplikasi golf/src/VIPMember.java
@@ -1,12 +1,17 @@
-public class VIPMember extends Member {
-    private double discount;
+    public class VIPMember extends Member {
+        private double discount;
 
-    public VIPMember(String name, String email, double discount) {
-        super(name, email);
-        this.discount = discount;
+        public VIPMember(String name, String email, double discount) {
+            super(name, email);
+            this.discount = discount;
+        }
+
+        @Override
+    public double calculateTotal(double originalPrice) {
+        return originalPrice * (1 - discount);
     }
 
-    public double getDiscount() {
-        return discount;
+        public double getDiscount() {
+            return discount;
+        }
     }
-}
